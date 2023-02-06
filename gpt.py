243a7b45cbf7c8deeeb95b853dc345073a6114ea
Bibit Kunwar <bibitkunwar@Bibits-MacBook-Pro.local>
2023-02-07 00:39:44 +0545

    Implement memory feature for previous conversation

--- gpt.py
+++ gpt.py
@@ -3,9 +3,12 @@ import openai
 # Set OpenAI API key
 openai.api_key = "your-api-key"
 
-def chatbot(prompt):
+def chatbot(prompt, history):
+     # Prepare the prompt by including the previous conversation history
+    prompt = "\n".join(history) + "\n" + prompt
+    
     completions = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=1024,
         n=1,
@@ -16,11 +19,14 @@ def chatbot(prompt):
     message = completions.choices[0].text
     return message
 
+history = []
 print("Hi, I am a chatbot powered by OpenAI GPT-3. How can I help you today?")
 
 while True:
     user_input = input("You: ")
-    if user_input == "exit":
+    if user_input.lower() == "exit":
         break
-    response = chatbot(user_input)
+    response = chatbot(user_input, history)
+    history.append("You: " + user_input)
+    history.append("Chatbot: " + response)
     print("Chatbot: " + response)
